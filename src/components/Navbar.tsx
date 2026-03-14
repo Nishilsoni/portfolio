094387b218ff8bc991f7797a7edd6cdb53dbbabf
Nishil Soni <soninishil27@gmail.com>
2026-03-14 11:57:17 +0530

    fixed render issues

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import HoverLinks from "./HoverLinks";
 import { gsap } from "gsap";
 import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { SplitText } from "gsap/SplitText";
 import "./styles/Navbar.css";
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
 
